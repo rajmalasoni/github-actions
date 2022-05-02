d5e50f23212cc7249cf624585a58c197cf4418c2
impressico-devOps <devops@impressico.com>
2022-05-02 09:38:57 +0530

    sapsrated event pull_request

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Composite-Actions
+
+This version for `pull_request` it will close PR if no description and feature branch targets to the master/main branch.
--- main.sh
+++ main.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
-echo $PR_NUMBER
-COMMENT="$COMMENT_BODY"
+echo "Target & Description Checker"
+PR_URL="$PR_URL"
 DESCRIPTION="$PR_BODY"
 BASE="$BASE_REF"
 HEAD="$HEAD_REF"
 SEP="&&"
-
-# Issue comments
-case $COMMENT in
-  /Close)
-    gh pr close $PR_URL
-    gh pr comment $PR_URL --body "Pull Request Closed!"
-    ;;
-  /Approved)
-    gh pr merge $PR_URL -m
-    gh pr comment $PR_URL --body "Pull Request Merged!"
-    ;;
-  *)
-  echo "For manually approve or close PR use slash-comments on body with /Approved or /Close"
-  ;;
-esac
 
 # target master
 case "${BASE}${SEP}${HEAD}" in
   "true${SEP}true") 
     echo "should not close PR target from release branch to master";;
   "false${SEP}false") 
-    echo "Should not close PR from other branch";;
+    echo "Should not close PR from feature branches";;
   "true${SEP}false") 
     gh pr close $PR_URL
     gh pr comment $PR_URL --body "PR from feature branch to master won't accept";;
@@ -35,6 +20,7 @@ esac
 
 # Description
 if [[ ! $DESCRIPTION ]]; then
+  echo "PR has No valied description"
   gh pr close $PR_URL
   gh pr comment $PR_URL --body "No Description on PR body. Please add valied description."  
 else
