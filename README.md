317042be19a29c3885340a3de57bbef58da323dc
NaveenKundeti-IBS <kundeti.naveen@impressico.com>
2022-05-09 19:26:43 +0530

    updated stale actions

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Composite-Actions
+# Composite-Actions for Close Stale PRs
 
-Composite actions for stale/inactive PR
+Closes PRs that have had no activity for a specified amount of time.
+
+The configuration must be on the default branch and the default values will:
+
+- Add a label "Stale" on pull requests after 10 days of inactivity and comment on them
+- Close the stale pull requests after 4 days of inactivity
+- If an update/comment occur on stale pull requests, the stale label will be removed and the timer will restart
+
+## Recommended permissions
+
+For the execution of this action, it must be able to fetch all pull requests from our repository.  
+In addition, based on the provided configuration, the action could require more permission(s) (e.g.: add label, remove label, comment, close, etc.).  
+
+```yaml
+permissions: write-all
+
+```
--- main.sh
+++ main.sh
@@ -2,36 +2,82 @@
 
 PR_URL="$PR_URL"
 token="$GITHUB_TOKEN"
+BASE_URI="https://api.github.com"
 owner="$REPO_OWNER"
 repo="$REPO_NAME"
 pull_number="$PR_NUMBER"
 
 #time
-req_time=864000 #10 days
 aday=86400 #24 hrs
+four_days=345600
+nine_days=777600
+ten_days=864000
+
+e=60 # 1 mins
+f=180 #3 mins
+g=300 #5 mins
+
 
 #date and time of PR
 
-latest_commit_date=$(curl -X GET -u devops-ibs:$token https://api.github.com/repos/$owner/$repo/pulls/$pull_number/commits | jq -r '.[-1].commit.committer.date')
+latest_commit_date=$(curl -X GET -u devops-ibs:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number/commits | jq -r '.[-1].commit.committer.date')
+stale_date=$(curl -X GET -u devops-ibs:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number | jq -r '.updated_at')
+
 live_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 convert_live_date=$(date -u -d "$live_date" +%s)
 convert_latest_commit_date=$(date -u -d "$latest_commit_date" +%s)
+convert_stale_date=$(date -u -d "$stale_date" +%s)
 DIFFERENCE=$((convert_live_date - convert_latest_commit_date))
+label_diff=$((convert_live_date - convert_stale_date))
 
 echo "latest commit date: $latest_commit_date"
+echo "stale label date: $stale_date"
 echo "live date: $live_date"
 echo "convert live date: $convert_live_date"
 echo "convert latest commit date: $convert_latest_commit_date"
+echo "convert stale label date: $convert_stale_date"  
 echo "difference time: $DIFFERENCE"
+echo "label difference time: $label_diff"
 
-if [ $DIFFERENCE -lt $aday ]
+
+if [ $DIFFERENCE -lt $e ]
 then
    echo "This PR is active. Don't close PR"
-elif [ $DIFFERENCE -gt $aday ]
+   gh pr edit $PR_URL --remove-label "Stale"
+elif [ $DIFFERENCE -le $f ]
 then
-   echo "This PR is stale becacuse no activity. Close PR"
-   # gh pr close $PR_URL
-   # gh pr comment $PR_URL --body "Pull Request is stale"
+   echo "This PR is stale because it has been open 10 days with no activity."
+   gh pr edit $PR_URL --add-label "Stale" 
+   gh pr comment $PR_URL --body "This issue is stale because it has been open 10 days with no activity. Remove stale label or comment or this will be closed in 4 days."
+
+elif [ $label_diff -gt $f ]
+then
+   echo "This PR was closed because it has been stalled for 4 days with no activity."
+   gh pr close $PR_URL
+   gh pr edit $PR_URL --remove-label "Stale"
+   gh pr comment $PR_URL --body "This PR was closed because it has been stalled for 4 days with no activity."
+ 
 else
    echo "None of the condition met"
 fi
+
+
+# if [ $DIFFERENCE -lt $nine_days ]
+# then
+#    echo "This PR is active. Don't close PR"
+#    gh pr edit $PR_URL --remove-label "Stale"
+# elif [ $DIFFERENCE -le $ten_days ]
+# then
+#    echo "This PR is stale because it has been open 10 days with no activity."
+#    gh pr edit $PR_URL --add-label "Stale" 
+#    gh pr comment $PR_URL --body "This issue is stale because it has been open 10 days with no activity. Remove stale label or comment or this will be closed in 4 days."
+# elif [ $label_diff -gt $four_days ]
+# then
+#    echo "This PR was closed because it has been stalled for 4 days with no activity."
+#    gh pr close $PR_URL
+#    gh pr edit $PR_URL --remove-label "Stale"
+#    gh pr comment $PR_URL --body "This PR was closed because it has been stalled for 4 days with no activity."
+ 
+# else
+#    echo "None of the condition met"
+# fi
