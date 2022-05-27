0985e3924cc397e66c6e2da2d33e5073cb32d477
NaveenKundeti-IBS <kundeti.naveen@impressico.com>
2022-05-27 13:38:50 +0530

    updated pr stale date

--- main.sh
+++ main.sh
@@ -19,17 +19,17 @@ pull_number="$PR_NUMBER"
 stale () {
 
 #date and time of PR
-latest_commit_date=$(curl -X GET -u $owner:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number/commits | jq -r '.[-1].commit.committer.date')
+pr_updated_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number | jq -r '.updated_at')
 
 live_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 convert_live_date=$(date -u -d "$live_date" +%s)
-convert_latest_commit_date=$(date -u -d "$latest_commit_date" +%s)
-DIFFERENCE=$((convert_live_date - convert_latest_commit_date))
+convert_pr_updated_at=$(date -u -d "$pr_updated_at" +%s)
+DIFFERENCE=$((convert_live_date - convert_pr_updated_at))
 
-echo "latest commit date: $latest_commit_date"
 echo "live date: $live_date"
 echo "convert live date: $convert_live_date"
-echo "convert latest commit date: $convert_latest_commit_date"
+echo "pr updated at: $pr_updated_at"
+echo "convert pr updated date: $convert_pr_updated_at"  
 echo "difference time: $DIFFERENCE"
 
 #time
