ec16c42a39af11e61288f44aeda487d6e27c9eb0
NaveenKundeti-IBS <kundeti.naveen@impressico.com>
2022-06-07 10:35:35 +0530

    Update stale days and label

--- main.sh
+++ main.sh
@@ -7,6 +7,7 @@ BASE="$BASE_REF"
 HEAD="$HEAD_REF"
 SEP="&&"
 STALE_DAYS=$STALE_DAYS
+CLOSE_DAYS=$CLOSE_DAYS
 
 # for curl API
 token="$GITHUB_TOKEN"
@@ -22,13 +23,15 @@ pr_updated_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r
 
 pr_number=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[-1].url')
 comments_url=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[-1].comments_url')
+label=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/issues | jq -r '.[-1].url')
 
 live_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 convert_live_date=$(date -u -d "$live_date" +%s)
 convert_pr_updated_at=$(date -u -d "$pr_updated_at" +%s)
 DIFFERENCE=$((convert_live_date - convert_pr_updated_at))
 SECONDSPERDAY=86400
-STALE_CLOSE=$(( STALE_DAYS * SECONDSPERDAY ))
+STALE_LABEL=$(( STALE_DAYS * SECONDSPERDAY ))
+STALE_CLOSE=$(( CLOSE_DAYS * SECONDSPERDAY ))
 
 echo "live date: $live_date"
 echo "convert live date: $convert_live_date"
@@ -39,16 +42,22 @@ echo "pr number: $pr_number"
 echo "Days Before Close in seconds: $STALE_CLOSE"
 
 case $((
-(DIFFERENCE < STALE_CLOSE) * 1 +
+(DIFFERENCE < STALE_LABEL) * 1 +
 (DIFFERENCE > STALE_CLOSE) * 2)) in
-(1) echo "This PR is active." ;;
+(1) echo "This PR is active."
+  curl -X POST -u $owner:$token $label \
+  -d '{ "labels":["Stale"] }'
+
+  curl -X POST -u $owner:$token $comments_url \
+  -d '{"body":"This PR is stale because it has been open 15 days with no activity. Remove stale label or comment or this will be closed in 2 days."}' 
+;;
 (2) echo "This PR is stale and close because it has been open from $STALE_DAYS days with no activity."
 
   curl -X PATCH -u $owner:$token $pr_number \
   -d '{ "state": "closed" }'
 
   curl -X POST -u $owner:$token $comments_url \
-  -d '{"body":"This PR was closed because it has been stalled and with no activity."}'
+  -d '{"body":"This PR was closed because it has been stalled for 2 days with no activity."}'
   ;;
 esac  
 
