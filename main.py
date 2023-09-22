2a53a31d5c9a1b4d7fb270507faa1ca72e50155f
arun <arun.kumar@impressico.com>
2023-09-22 12:34:08 +0530

    update the workflow

--- main.py
+++ main.py
@@ -3,57 +3,57 @@ from github import Github
 from datetime import datetime, timedelta
 import requests
 
-# env values
-g = Github(os.environ["GITHUB_TOKEN"])
-repo = g.get_repo(os.environ['REPO_NAME'])
-pulls = repo.get_pulls(state='open')
-
-pr_number = int(os.environ['PR_NUMBER']) if ( os.environ['PR_NUMBER'] ) else None;
-pr = repo.get_pull(pr_number) if(pr_number) else None;
-
-MERGE_PR = os.environ.get("MERGE_PR")
-CLOSE_PR = os.environ.get("CLOSE_PR")
-VERSION_FILE = os.environ.get("VERSION_FILE")
-EVENT = os.environ['EVENT']
-GCHAT_WEBHOOK_URL = os.environ['WEBHOOK']
-
-# Global variables
-# 2 stale PR
-stale_days = 15
-# 3.close staled PR if 2 days of no activity
-stale_close_days = 2
-
-#MESSAGES
-# 2 stale PR 
-msg_job2 = 'This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.'
-# 3.close staled PR if 2 days of no activity
-msg_job3 = 'This PR was closed because it has been stalled for 2 days with no activity.'
-# 4.Check if the pull request targets the master branch directly
-msg_job4 = 'Do not accept PR target from feature branch to master branch.'
-# 5.Check if the pull request has a description
-msg_job5 = 'No Description on PR body. Please add valid description.'
-# 6_1 Check if the Approved comment in the pull request comments
-commit_message_job6 = 'Pull Request Approved and Merged!'
-msg_job6_1 = 'This pull request was approved and merged because of a slash command.'
-# 6_2 Check if the Close comment in the pull request comments
-msg_job6_2 = 'This pull request was closed because of a slash command.'
-# 7. Check All the files and see if there is a file named "VERSION"
-msg_job7_success = 'The VERSION file exists. All ohk'
-msg_job7_reject = "The VERSION file does not exist. Closing this pull request."
-# 8. Check if version name from "VERSION" already exists as tag  
-msg_job8_success = "The VERSION didnt matched with tag. All ok"
-msg_job8_reject = "The tag from VERSION file already exists. Please update the VERSION file."
-# 9. message need to be placed here
-if(pr):
-    msg_job9_opened = f"New Pull Request Created by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
-    msg_job9_edited = f"Pull Request Edited by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
-    msg_job9_closed = f"Pull Request Closed by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
-    msg_job9_reopened = f"Pull Request Reopened by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
-
-print("repo:",repo)
-print("pulls:",pulls)
-
 try:
+    # env values
+    g = Github(os.environ["GITHUB_TOKEN"])
+    repo = g.get_repo(os.environ['REPO_NAME'])
+    pulls = repo.get_pulls(state='open')
+
+    pr_number = int(os.environ['PR_NUMBER']) if ( os.environ['PR_NUMBER'] ) else None;
+    pr = repo.get_pull(pr_number) if(pr_number) else None;
+
+    MERGE_PR = os.environ.get("MERGE_PR")
+    CLOSE_PR = os.environ.get("CLOSE_PR")
+    VERSION_FILE = os.environ.get("VERSION_FILE")
+    EVENT = os.environ['EVENT']
+    GCHAT_WEBHOOK_URL = os.environ['WEBHOOK']
+
+    # Global variables
+    # 2 stale PR
+    stale_days = 15
+    # 3.close staled PR if 2 days of no activity
+    stale_close_days = 2
+
+    #MESSAGES
+    # 2 stale PR 
+    msg_job2 = 'This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.'
+    # 3.close staled PR if 2 days of no activity
+    msg_job3 = 'This PR was closed because it has been stalled for 2 days with no activity.'
+    # 4.Check if the pull request targets the master branch directly
+    msg_job4 = 'Do not accept PR target from feature branch to master branch.'
+    # 5.Check if the pull request has a description
+    msg_job5 = 'No Description on PR body. Please add valid description.'
+    # 6_1 Check if the Approved comment in the pull request comments
+    commit_message_job6 = 'Pull Request Approved and Merged!'
+    msg_job6_1 = 'This pull request was approved and merged because of a slash command.'
+    # 6_2 Check if the Close comment in the pull request comments
+    msg_job6_2 = 'This pull request was closed because of a slash command.'
+    # 7. Check All the files and see if there is a file named "VERSION"
+    msg_job7_success = 'The VERSION file exists. All ohk'
+    msg_job7_reject = "The VERSION file does not exist. Closing this pull request."
+    # 8. Check if version name from "VERSION" already exists as tag  
+    msg_job8_success = "The VERSION didnt matched with tag. All ok"
+    msg_job8_reject = "The tag from VERSION file already exists. Please update the VERSION file."
+    # 9. message need to be placed here
+    if(pr):
+        msg_job9_opened = f"New Pull Request Created by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
+        msg_job9_edited = f"Pull Request Edited by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
+        msg_job9_closed = f"Pull Request Closed by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
+        msg_job9_reopened = f"Pull Request Reopened by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}";
+
+    print("repo:",repo)
+    print("pulls:",pulls)
+
     # 1.Add "Stale" label to the PR if no active from 15 days
     now = datetime.now()
     for pr in pulls:
@@ -177,14 +177,5 @@ try:
         print(response)
         print(EVENT)
 
-
-    # if __name__ == '__main__':
-    #     print('start')
-    #     if MERGE_PR.__eq__('true'):
-    #         merge()  
-    #     if CLOSE_PR.__eq__('true'):
-    #         close()   
-    #     print('end')
 except Exception as e:
-    print(f"Failed to run the job. exception: {str(e)}")
-    print(f"PR_NUMBER : {os.environ['PR_NUMBER']}")      
+    print(f"Failed to run the job. exception: {str(e)}")      
